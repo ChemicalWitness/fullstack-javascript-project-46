5b2f0671447afc6bcbcb838c6196d8b8cbc9a4f8
Igor <trololol-lolo@mail.ru>
2025-04-14 22:50:49 +1000

    Шаг второй

--- gendiff.js
+++ gendiff.js
@@ -7,6 +7,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .helpOption('-h, --help', 'display help for command')
 
   program.parse()
