cf26257fa1b31ba1458e38bcc4f5e988cd05fde7
Igor <trololol-lolo@mail.ru>
2025-05-06 23:14:49 +1000

    Шаг 3

--- gendiff.js
+++ gendiff.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+import {parse, getFormat, readFile} from './parse.js';
 
 const program = new Command();
+
+const checkPath = (filepath1, filepath2) => {
+  const data1 = parse(readFile(filepath1), getFormat(filepath1))
+  const data2 = parse(readFile(filepath2), getFormat(filepath2))
+  console.log(data1)
+  console.log(data2)
+
+  return console.log('Comparing...')
+}
 
 program
   .name('gendiff')
@@ -10,5 +20,8 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .helpOption('-h, --help', 'display help for command')
+  .action((filepath1, filepath2) => {
+    return checkPath(filepath1, filepath2)
+  })
 
   program.parse()
---
+++ parse.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
+const readFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  return fs.readFileSync(absolutePath, 'utf-8');
+}
+
+const parse = (file, format) => {
+  return format === 'json' ? JSON.parse(file) : new Error('Invalid format')
+}
+
+const getFormat = (filepath) => {
+  const ext = path.extname(filepath).slice(1).toLowerCase();
+  return ext;
+}
+
+export {readFile, parse, getFormat}
