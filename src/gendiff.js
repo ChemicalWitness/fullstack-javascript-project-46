b880762260e46192bb183e748be4e244e712d458
Igor <trololol-lolo@mail.ru>
2025-05-19 11:59:00 +1000

    Шаг 4

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,13 +1,33 @@
+import _ from 'lodash';
 import { Command } from 'commander';
 import { parse, getFormat, readFile } from './parse.js';
 
 const checkPath = (filepath1, filepath2) => {
   const data1 = parse(readFile(filepath1), getFormat(filepath1));
   const data2 = parse(readFile(filepath2), getFormat(filepath2));
-  console.log(data1);
-  console.log(data2);
+  // console.log(data1);
+  // console.log(data2);
+  const keysObj1 = Object.keys(data1);
+  const keysObj2 = Object.keys(data2);
 
-  return console.log('Comparing...');
+  const uniqKeys = _.sortBy(Array.from(new Set([...keysObj1, ...keysObj2])));
+  const diffLines = uniqKeys.map((key) => {
+    const valueObj1 = data1[key];
+    const valueObj2 = data2[key];
+
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${valueObj1}`;
+    }
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${valueObj2}`;
+    }
+    if (!_.isEqual(valueObj1, valueObj2)) {
+      return `  - ${key}: ${valueObj1}\n  + ${key}: ${valueObj2}`;
+    }
+    return `  ${key}: ${valueObj1}`;
+  });
+
+  return console.log(`{\n${diffLines.join('\n')}\n}`);
 };
 
 const runCommander = () => {
