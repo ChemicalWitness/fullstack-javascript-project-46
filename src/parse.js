d356c32ab4166fb22a48ffc1a5b6fdbaeb4c0156
Igor <trololol-lolo@mail.ru>
2025-05-22 13:29:09 +1000

    Правки по линтеру

--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import path from 'path'
-import yaml from 'js-yaml';
+import yaml from 'js-yaml'
 
 const readFile = (filepath) => {
   const absolutePath = path.resolve(process.cwd(), filepath)
@@ -11,7 +11,8 @@ const parse = (file, format) => {
   // return format === 'json' ? JSON.parse(file) : new Error('Invalid format')
   if (format === 'json') {
     return JSON.parse(file)
-  } else if (format === 'yml' || format === 'yaml') {
+  }
+  else if (format === 'yml' || format === 'yaml') {
     return yaml.load(file)
   }
   return new Error('invalid format')
