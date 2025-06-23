c6ebc9dbd34775b583aba620e853d05d7bbe4211
Igor <trololol-lolo@mail.ru>
2025-06-24 00:22:39 +1000

    Правки по форматеру Plain

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,11 +9,18 @@ const __dirname = dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 
 describe('gendiff', () => {
-  test('should show differences between two JSON YAML files', () => {
+  test('should show differences between two JSON YAML files format stylish', () => {
     const file1 = getFixturePath('file1.1.json')
     const file2 = getFixturePath('file2.1.yaml')
     const expected = fs.readFileSync(getFixturePath('expected_diff2.txt'), 'utf-8').trim()
     expect(gendiff(file1, file2)).toEqual(expected)
+  }),
+  test('should show differences between two JSON YAML files format plain', () => {
+    const file1 = getFixturePath('file1.1.json')
+    const file2 = getFixturePath('file2.1.yaml')
+    const format = 'plain'
+    const expected = fs.readFileSync(getFixturePath('expected_diff_plain.txt'), 'utf-8').trim()
+    expect(gendiff(file1, file2, format)).toEqual(expected)
   }),
   test('should show differences betweew two files on json format', () => {
     const file1 = getFixturePath('file1.1.json')
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,7 +7,7 @@ const stringifyPlain = (value) => {
   if (typeof value === 'object') {
     return `[complex value]`
   }
-  return String(value)
+  return typeof value === 'boolean' ? `${value}` : `'${value}'`
 }
 
 const buildPath = (path, key) => path ? `${path}.${key}` : key
@@ -17,11 +17,11 @@ const formatPlain = (nodes, path = '') => {
     const currentPath = buildPath(path, node.key)
     switch (node.type) {
       case nodeTypes.REMOVE:
-        return `Property ${currentPath} was removed`
+        return `Property '${currentPath}' was removed`
       case nodeTypes.ADDED:
-        return `Property ${currentPath} was added with value: ${stringifyPlain(node.valueObj2)}`
+        return `Property '${currentPath}' was added with value: ${stringifyPlain(node.valueObj2)}`
       case nodeTypes.CHANGE:
-        return `Property ${currentPath} was updated. From ${stringifyPlain(node.valueObj1)} to ${stringifyPlain(node.valueObj2)}`
+        return `Property '${currentPath}' was updated. From ${stringifyPlain(node.valueObj1)} to ${stringifyPlain(node.valueObj2)}`
       case nodeTypes.NESTED:
         return formatPlain(node.children, currentPath)
       case nodeTypes.UNCHANGED:
