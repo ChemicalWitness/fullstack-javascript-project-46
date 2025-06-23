f8fb635213ccc261924c756a923163c618b01f15
Igor <trololol-lolo@mail.ru>
2025-06-23 16:14:35 +1000

    Добавляет два форматера

--- src/formatter.js
+++ src/formatter.js
@@ -1,6 +1,6 @@
 import { nodeTypes } from './gendiff.js'
 
-const stringify = (value, depth) => {
+const stringifyStylish = (value, depth) => {
   if (value === null) {
     return 'null'
   }
@@ -12,12 +12,24 @@ const stringify = (value, depth) => {
   const currentIndent = ' '.repeat(indentSize)
   const bracketIndent = ' '.repeat(indentSize - 4)
   const lines = Object.entries(value).map(
-    ([key, val]) => `${currentIndent}${key}: ${stringify(val, depth + 1)}`,
+    ([key, val]) => `${currentIndent}${key}: ${stringifyStylish(val, depth + 1)}`,
   )
+  console.log(lines)
 
   return ['{', ...lines, `${bracketIndent}}`].join('\n')
 }
 
+const stringifyPlain = (value) => {
+  if (value === null) {
+    return null
+  }
+  if (typeof value === 'object') {
+    return `[complex value]`
+  }
+  return String(value)
+}
+
+const buildPath = (path, key) => path ? `${path}.${key}` : key
 const formatStylish = (nodes, depth = 1) => {
   const indentSize = depth * 4
   const currentIndent = ' '.repeat(indentSize - 2)
@@ -25,27 +37,47 @@ const formatStylish = (nodes, depth = 1) => {
   const diff = nodes.flatMap((node) => {
     switch (node.type) {
       case nodeTypes.REMOVE:
-        return `${currentIndent}- ${node.key}: ${stringify(node.valueObj1, depth + 1)}`
+        return `${currentIndent}- ${node.key}: ${stringifyStylish(node.valueObj1, depth + 1)}`
       case nodeTypes.ADDED:
-        return `${currentIndent}+ ${node.key}: ${stringify(node.valueObj2, depth + 1)}`
+        return `${currentIndent}+ ${node.key}: ${stringifyStylish(node.valueObj2, depth + 1)}`
       case nodeTypes.CHANGE:
         return [
-          `${currentIndent}- ${node.key}: ${stringify(node.valueObj1, depth + 1)}`,
-          `${currentIndent}+ ${node.key}: ${stringify(node.valueObj2, depth + 1)}`,
+          `${currentIndent}- ${node.key}: ${stringifyStylish(node.valueObj1, depth + 1)}`,
+          `${currentIndent}+ ${node.key}: ${stringifyStylish(node.valueObj2, depth + 1)}`,
         ]
       case nodeTypes.NESTED:
         return `${currentIndent}  ${node.key}: ${formatter(node.children, depth + 1)}`
       default:
-        return `${currentIndent}  ${node.key}: ${stringify(node.valueObj2, depth + 1)}`
+        return `${currentIndent}  ${node.key}: ${stringifyStylish(node.valueObj2, depth + 1)}`
     }
   })
 
   return [`{`, ...diff, `${bracketIndent}}`].join('\n')
 }
 
+const formatPlain = (nodes, path = '') => {
+  const diff = nodes.flatMap((node) => {
+    const currentPath = buildPath(path, node.key);
+    switch (node.type) {
+      case nodeTypes.REMOVE:
+        return `Property ${currentPath} was removed`
+      case nodeTypes.ADDED:
+        return `Property ${currentPath} was added with value: ${stringifyPlain(node.valueObj2)}`
+      case nodeTypes.CHANGE:
+        return `Property ${currentPath} was updated. From ${stringifyPlain(node.valueObj1)} to ${stringifyPlain(node.valueObj2)}`
+      case nodeTypes.NESTED:
+        return formatPlain(node.children, currentPath)
+      case nodeTypes.UNCHANGED:
+        return []
+    }
+  })
+
+  return diff.join('\n')
+}
+
 const formatter = (nodes, depth, format = 'stylish') => {
   if (format != 'stylish') {
-    throw new Error('Uncorrect format')
+    return formatPlain(nodes)
   }
   return formatStylish(nodes, depth)
 }
