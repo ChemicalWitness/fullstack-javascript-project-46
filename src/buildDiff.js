f881400e7df2c723ce622b68ed008bcfedae6ad4
Igor <trololol-lolo@mail.ru>
2025-07-26 14:00:20 +1000

    Меняет проверку на существование ключа в объекте

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -11,16 +11,17 @@ const nodeTypes = {
 const buildDiff = (data1, data2) => {
   const keysObj1 = Object.keys(data1)
   const keysObj2 = Object.keys(data2)
+  console.log(keysObj1)
 
   const uniqKeys = _.sortBy(Array.from(new Set([...keysObj1, ...keysObj2])))
   const diffLines = uniqKeys.map((key) => {
     const valueObj1 = data1[key]
     const valueObj2 = data2[key]
 
-    if (!_.includes(keysObj2, key)) {
+    if (!Object.hasOwn(data2, key)) {
       return { type: nodeTypes.REMOVE, key, valueObj1 }
     }
-    if (!_.includes(keysObj1, key)) {
+    if (!Object.hasOwn(data1, key)) {
       return { type: nodeTypes.ADDED, key, valueObj2 }
     }
     if (_.isPlainObject(valueObj1) && _.isPlainObject(valueObj2)) {
